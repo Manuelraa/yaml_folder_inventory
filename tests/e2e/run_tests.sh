1689e33e54c5c416b1bea8a0ade1e25a0b86324d
Manuelraa <manuel@bloodycrystals.de>
2022-02-02 00:29:37 +0100

    Add second test and fix diffrent output for 2.7 and bellow

--- tests/e2e/run_tests.sh
+++ tests/e2e/run_tests.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+# Since ansible 2.8 in the output empty objects are removed
+ansible_version=$(pip list 2>/dev/null | grep "ansible " | tr  -s ' ' | cut -d ' ' -f 2)
+if [[ "$ansible_version" == "2.5."* || "$ansible_version" == "2.6."* || "$ansible_version" == "2.7."* ]]; then
+    expected_file='old-expected.json'
+else
+    expected_file='expected.json'
+fi
+
 for folder in $(find -maxdepth 1 -type d -not -name '.'); do
-    echo -n "Testing: $folder -> "
+    echo "Testing: $folder"
     # 'ungrouped' key was added in 2.8
     # Its always empty for our tests so just remove it
-    ansible-inventory --list -i "$folder/inventory/yaml_folder.yml" | jq --sort-keys 'del(.ungrouped)' > result.json
-    diff <(jq --sort-keys . "$folder/expected.json") result.json
-    if [[ $? -eq 0 ]]; then
+    ansible-inventory --list -i "$folder/inventory/yaml_folder.yml" | jq --sort-keys '.' > result.json
+    echo "Diff < expected -- result >"
+    diff <(jq --sort-keys '.' "$folder/$expected_file") result.json
+    diff_exit_code=$?
+    rm result.json
+    if [[ $diff_exit_code -eq 0 ]]; then
         echo "Success"
     else
         echo "Failed"
