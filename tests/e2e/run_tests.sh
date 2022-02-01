952e88093efa65bc9ebfaba349364640f61d853a
Manuelraa <manuel@bloodycrystals.de>
2022-02-02 00:45:18 +0100

    Cleanup tox config and allow parallel tests

--- tests/e2e/run_tests.sh
+++ tests/e2e/run_tests.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+result_file="result-$RANDOM.json"
+python_version=$(python -V | cut -d ' ' -f 2)
+
 # Since ansible 2.8 in the output empty objects are removed
 ansible_version=$(pip list 2>/dev/null | grep "ansible " | tr  -s ' ' | cut -d ' ' -f 2)
 if [[ "$ansible_version" == "2.5."* || "$ansible_version" == "2.6."* || "$ansible_version" == "2.7."* ]]; then
@@ -9,15 +12,17 @@ else
     expected_file='expected.json'
 fi
 
+echo "Python: $python_version / Ansible: $ansible_version"
+
 for folder in $(find -maxdepth 1 -type d -not -name '.'); do
     echo "Testing: $folder"
     # 'ungrouped' key was added in 2.8
     # Its always empty for our tests so just remove it
-    ansible-inventory --list -i "$folder/inventory/yaml_folder.yml" | jq --sort-keys '.' > result.json
+    ansible-inventory --list -i "$folder/inventory/yaml_folder.yml" | jq --sort-keys '.' > "$result_file"
     echo "Diff < expected -- result >"
-    diff <(jq --sort-keys '.' "$folder/$expected_file") result.json
+    diff <(jq --sort-keys '.' "$folder/$expected_file") "$result_file"
     diff_exit_code=$?
-    rm result.json
+    rm "$result_file"
     if [[ $diff_exit_code -eq 0 ]]; then
         echo "Success"
     else
